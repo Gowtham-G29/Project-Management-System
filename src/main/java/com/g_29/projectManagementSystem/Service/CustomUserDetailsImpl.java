c21c4e9c45f756aafab15fe2dbcfad6b9f4dccc4
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-06-16 12:53:34 +0530

    Implement Custom user from loading the database using userDetailsService

---
+++ src/main/java/com/g_29/projectManagementSystem/Service/CustomUserDetailsImpl.java
@@ -0,0 +1,38 @@
+package com.g_29.projectManagementSystem.Service;
+
+import com.g_29.projectManagementSystem.Repository.UserRepo;
+import com.g_29.projectManagementSystem.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//this enables the custom login details from the db.
+// and disable spring security default generated password.
+@Service
+public class CustomUserDetailsImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user=userRepo.findByEmail(username);
+
+        if (user==null){
+            throw new UsernameNotFoundException("User not found in this email");
+        }
+        //here we are not using any authorities so just pass a empty authorities
+        List<GrantedAuthority>authorities=new ArrayList<>();
+
+        //this will load the Custom user from the database instead of Hardcoded user.
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+
+    }
+}
---
+++ src/main/java/com/g_29/projectManagementSystem/Service/UserService.java
@@ -0,0 +1,9 @@
+package com.g_29.projectManagementSystem.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
