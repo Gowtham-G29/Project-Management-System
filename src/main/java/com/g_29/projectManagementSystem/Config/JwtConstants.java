f8667f73dd501d90a53a17e6f4aeb9e95ac0d274
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-06-16 12:18:08 +0530

    Implement the JwtTokenValidator filter

---
+++ src/main/java/com/g_29/projectManagementSystem/Config/JwtConstants.java
@@ -0,0 +1,7 @@
+package com.g_29.projectManagementSystem.Config;
+
+public class JwtConstants {
+
+    public static final String SECREAT_KEY="ggoowwtthhaammgguurruuvvaayyuurraappaann";
+    public static final String JWT_HEADER="AUTHORIZATION";
+}
